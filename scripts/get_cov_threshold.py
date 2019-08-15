780f9805a2321428790e564d9b3719065aab7945
Alexis <alexis.simon@normalesup.org>
2019-08-15 17:56:46 +0200

    correction

--- scripts/get_cov_threshold.py
+++ scripts/get_cov_threshold.py
@@ -7,21 +7,21 @@ import pandas as pd
 # input is a file containing a list of all mosdepth.global.dist.txt files
 
 coverage_quantile = float(snakemake.params.coverage_quantile)
-
+input_files = snakemake.input
+output = str(snakemake.output)
 
 def get_max_cov(path, q):
     tb = pd.read_csv(path, sep="\t", header=None, names=['chrom', 'cov', 'p'])
     # chromosome, coverage, proportion of bases at this coverage
     tot = tb[tb.chrom == 'total']
-    max_cov = min(tot[tot.p < q]['cov'])
+    max_cov = min(tot[tot.p < (1 - q)]['cov'])
     return(max_cov)
-
 
 # with open(file_list, 'r') as f:
 # for file in f.readlines():
-with open(str(snakemake.output), 'w') as outfile:
-    for file in snakemake.input:
+with open(output, 'w') as outfile:
+    for file in input_files:
         filepath = PurePath(file.strip())
         max_cov = get_max_cov(filepath, coverage_quantile)
         sample = filepath.stem.replace('.mosdepth.global.dist', '')
-        outfile.write(sample + '\t' + str(max_cov))
+        outfile.write(sample + '\t' + str(max_cov) + '\n')
