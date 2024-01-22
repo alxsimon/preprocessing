ff0addfb7965a9cbf97ee96a4dd9dad266fd5da4
Alexis Simon <alexis.simon@normalesup.org>
2024-01-23 00:55:02 +0100

    Change the reference genome

---
+++ launch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+for batch in {1..10}; do
+  snakemake --use-singularity --use-conda \
+  --singularity-args "-B /data2:/data2" \
+  --rerun-incomplete \
+  -c 64 --batch preprocessing=${batch}/10 \
+  $1 \
+  preprocessing
+done
+
+# snakemake --use-singularity --use-conda \
+# --singularity-args "-B /data2:/data2" \
+# --rerun-incomplete \
+# -c 64 --batch preprocessing=1/10 \
+# $1 \
+# preprocessing
