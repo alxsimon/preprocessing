c40af2b6d3b18ddec276125d028baf10a5edbed8
Alexis Simon <alexis.simon@normalesup.org>
2024-03-15 22:10:14 +0100

    modifs to map to trosulus ref genome

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 for batch in {1..10}; do
   snakemake --use-singularity --use-conda \
-  --singularity-args "-B /data2:/data2" \
+  --singularity-args "-B /data2:/data2 -B /data1:/data1" \
   --rerun-incomplete \
   -c 64 --batch preprocessing=${batch}/10 \
   $1 \
